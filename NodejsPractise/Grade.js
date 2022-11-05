e7ad84846d2a26c570c8ab6f40099d8625a03f0f
Akash Sadawarti <sadawarti.akash@gmail.com>
2022-11-05 14:36:13 +0530

    Node js Basic Program

---
+++ NodejsPractise/Grade.js
@@ -0,0 +1,16 @@
+// var a = 40, b =  1, c= 2;
+//  var Total = a+b+c;
+
+//         if(Total < 45)
+//         {
+//             console.log("Addition is less than 45");
+//         }
+//         else 
+//         {
+//             console.log("Addition is greater than 45");
+//         }
+        
+    
+var array = [10,20,30];
+var reversearray =array.reverse();
+console.log(reversearray);      
