3218ed6b117e3275dd259b420316e6ba178a3f5e
Akash Sadawarti <sadawarti.akash@gmail.com>
2022-11-07 12:04:22 +0530

    Function practise

---
+++ NodejsPractise/FunctionPractise.js
@@ -0,0 +1,27 @@
+let a = 20;
+let b = 30
+let add = a+b;
+let print = add();
+// function add(){
+//     return a+b;
+// }
+  //console.log(a + b);
+  console.log(a * b);
+  console.log(a / b);
+  console.log(a - b);
+
+a,b => a+b;
+a,b => a*b;
+a,b => a/b;
+a,b => a-b;
+
+console.log(a+b);
+a,b => {if(a+b < 60)
+{
+ console.log("Addition is less than 60");
+}
+else 
+{
+    console.log("Addition is greater than 60");
+}
+}
