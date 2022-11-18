8667642ee000db204000f672c9cba325ec2b4b2f
Akash Sadawarti <sadawarti.akash@gmail.com>
2022-11-18 12:17:54 +0530

    Assignment 2of10 Sum of 1st 10 Natural Numbers

---
+++ Assignment2of10/Assignment2of10.js
@@ -0,0 +1,30 @@
+//2. Write a program to calculate the sum of first 10 natural number.
+// soln =>
+//By User Input
+// let num = prompt("Enter a number")
+// let newNum = parseInt(num)
+// console.log(`Sum of natural numbers 1 to ${newNum} is` )
+
+// function getNaturalNumbers(){
+  
+//         let sum = 0;
+//         sum = (newNum*(newNum+1))/2
+//         console.log(sum);
+   
+// }
+
+// getNaturalNumbers();
+
+//By static value
+let num = 10
+console.log(`Sum of first 10 natural numbers is` )
+
+function getNaturalNumbers(){
+  
+        let sum = 0;
+        sum = (num*(num+1))/2
+        console.log(sum);
+   
+}
+
+getNaturalNumbers();
