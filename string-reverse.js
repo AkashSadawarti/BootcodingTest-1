a87cbc9927e9d70b28b04b4b2eb7d4b9a504ac77
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-02-04 12:19:18 +0530

    reversing string

---
+++ string-reverse.js
@@ -0,0 +1,11 @@
+
+    //Reverse the given sentence "I am learning javascript in Bootcoding"
+  
+      let stringtype =  'I am learning javascript in Bootcoding'
+
+      function getReverseString(stringtype){
+            let result = stringtype.split(' ').reverse()
+            return result;
+      }
+      console.log( getReverseString(stringtype));
+    
