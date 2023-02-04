dc08c07895130f839e64b436f14fcb9b96db3352
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-02-04 12:35:51 +0530

    Grades program and Js arrow functions

--- NodejsPractise/Grade.js
+++ NodejsPractise/Grade.js
@@ -10,7 +10,38 @@
 //             console.log("Addition is greater than 45");
 //         }
         
-    
-var array = [10,20,30];
-var reversearray =array.reverse();
-console.log(reversearray);      
+ // reverse method in js   
+// var array = [10,20,30];
+// var reversearray =array.reverse();
+// console.log(reversearray);     
+
+a = 20;
+b = 30;
+let Addition = add(a,b);
+console.log(Addition);
+function add(a,b)
+{
+   return a+b;
+} 
+
+let Subtraction = sub(a,b);
+console.log(Subtraction);
+function sub(a,b)
+{
+    return b-a ;
+}
+
+
+ let Mult = (a,b) => (a*b);
+ let Multiply = Mult(a,b);
+console.log(Multiply);
+
+let Div = (a,b) => (a/b);
+let Division = Div(a,b);
+console.log(Division);
+
+var School = ['student','teachers','principal','peons','Drivers']
+for(i=0; i< School.length ; i++)
+{
+    console.log(School.length -3);
+}
