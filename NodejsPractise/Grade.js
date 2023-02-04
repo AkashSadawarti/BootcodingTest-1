b9e9612b1c1a8d5b116c5dbc5113a1c1395f1d7a
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-02-04 13:13:59 +0530

    Grade program update

--- NodejsPractise/Grade.js
+++ NodejsPractise/Grade.js
@@ -1,47 +1,62 @@
-// var a = 40, b =  1, c= 2;
-//  var Total = a+b+c;
+var physics, chemistry, maths;
+function getGrade(physics, chemistry, maths) {
+    var Total = physics + chemistry + maths;
+    let percentage = Total / 300 * 100
+    if (percentage < 100) {
+        if (percentage > 80) {
+            console.log("Student grade is A+ Extinction");
+        }
+        else if (percentage > 70 && percentage < 80) {
+            console.log("Student grade is A Good");
+        }
+        else if (percentage > 60 && percentage < 70) {
+            console.log("Student grade is B Average");
+        }
+        else if (percentage > 40 && percentage < 60) {
+            console.log("Student grade is C Average");
+        }
+        else if (percentage < 40) {
+            console.log("Student is Failed");
+        }
+    }
+    else {
+        console.log('Data is Undefined')
+    }
+}
 
-//         if(Total < 45)
-//         {
-//             console.log("Addition is less than 45");
-//         }
-//         else 
-//         {
-//             console.log("Addition is greater than 45");
-//         }
-        
- // reverse method in js   
+getGrade(102,107,130)
+
+
+
+// reverse method in js   
 // var array = [10,20,30];
 // var reversearray =array.reverse();
 // console.log(reversearray);     
 
-a = 20;
-b = 30;
-let Addition = add(a,b);
-console.log(Addition);
-function add(a,b)
-{
-   return a+b;
-} 
+// a = 20;
+// b = 30;
+// let Addition = add(a, b);
+// console.log(Addition);
+// function add(a, b) {
+//     return a + b;
+// }
 
-let Subtraction = sub(a,b);
-console.log(Subtraction);
-function sub(a,b)
-{
-    return b-a ;
-}
+// let Subtraction = sub(a, b);
+// console.log(Subtraction);
+// function sub(a, b) {
+//     return b - a;
+// }
 
 
- let Mult = (a,b) => (a*b);
- let Multiply = Mult(a,b);
-console.log(Multiply);
+// let Mult = (a, b) => (a * b);
+// let Multiply = Mult(a, b);
+// console.log(Multiply);
 
-let Div = (a,b) => (a/b);
-let Division = Div(a,b);
-console.log(Division);
+// let Div = (a, b) => (a / b);
+// let Division = Div(a, b);
+// console.log(Division);
 
-var School = ['student','teachers','principal','peons','Drivers']
-for(i=0; i< School.length ; i++)
-{
-    console.log(School.length -3);
-}
+// var School = ['student', 'teachers', 'principal', 'peons', 'Drivers']
+// for (i = 0; i < School.length; i++) {
+//     console.log(School.length - 3);
+// }
